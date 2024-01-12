e5db8171a0fd067a08462a50c10eb29b21e5eed1
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-01-12 17:51:18 +0900

    entity update

---
+++ src/main/java/blog/createblog/domain/Article.java
@@ -0,0 +1,19 @@
+package blog.createblog.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+}
