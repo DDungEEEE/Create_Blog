48894e39b771a67fed9520cb9ed7c78df0ce256b
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-01-12 17:20:00 +0900

    start Project

---
+++ src/test/java/blog/createblog/CreateBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.createblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
