59e1326520451154d7ed00d040f7b9076755ea7c
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-02-06 17:02:15 +0900

    User Service Create

--- src/main/java/blog/createblog/controller/UserApiController.java
+++ src/main/java/blog/createblog/controller/UserApiController.java
@@ -2,17 +2,32 @@ package blog.createblog.controller;
 
 import blog.createblog.dto.AddUserRequest;
 import blog.createblog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.parameters.P;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
+@Controller
 public class UserApiController {
     private final UserService userService;
 
     @PostMapping("/user")
     public String signup(AddUserRequest request){
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
